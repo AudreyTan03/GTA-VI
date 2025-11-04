834b9175a8891e85232aa17b6e3ae2eee5110f3c
AudreyTan03 <avtan2@student.hau.edu.ph>
2025-11-04 10:42:20 +0800

    Clean up the boilerplate, add the assets and set up the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger); // para di mo na need register isa isa pede mo gawin to
+
+function App() {
+  return (
+    <div>
+
+        <h1 className="text-3xl text-indigo-600"> WELKUM BA</h1>
+
+    </div>
+  )
+}
+
+export default App
